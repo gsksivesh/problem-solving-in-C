cc808cde425fb07fb84e3ae8750746f8cd6896e1
G S K Sivesh <gsksivesh@gmail.com>
2016-07-02 16:44:35 +0530

    Add files via upload

---
+++ combinations_of_string.cpp
@@ -0,0 +1,22 @@
+#include "combinations_of_string.h"
+
+void combinations(char *str)
+{
+	//2^strlen(str) ~= 1 << strlen(str)
+	for (int i = 1; i < (1 << strlen(str)); i++)
+	{
+		int temp = i;
+		int index = 0;
+		//Characters of index in which the bits are set in temp are printed
+		while (temp != 0)
+		{
+			if (temp & 1 == 1)
+			{
+				printf("%c", str[index]);
+			}
+			index++;
+			temp = temp >> 1;
+		}
+		printf("\n");
+	}
+}
