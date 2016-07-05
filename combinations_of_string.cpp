3a92e086f8c31bf52be3cd4a052228e8bbdc603a
G S K Sivesh <gsksivesh@gmail.com>
2016-07-05 23:16:04 +0530

    Update combinations_of_string.cpp

--- combinations_of_string.cpp
+++ combinations_of_string.cpp
@@ -1,8 +1,31 @@
-#include<stdio.h>
-#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
 
-void combinations(char *str)
+
+void delete_duplicates(char *given_str, char *str)
 {
+	int *alph, str_index = 0;
+	alph = (int*)malloc(sizeof(int)* 26);
+	memset(alph, 0, sizeof(int)* 26);
+	for (int i = 0; given_str[i] != '\0'; i++)
+	{
+		int val = given_str[i] - 'a';
+		if (alph[val] == 0)
+		{
+			alph[val] = 1;
+			str[str_index++] = given_str[i];
+		}
+	}
+	str[str_index] = '\0';
+	free(alph);
+}
+
+void combinations(char *given_str)
+{
+	char *str;
+	str = (char*)malloc(sizeof(char)* 26);
+	//remove duplicates preserving order
+	delete_duplicates(given_str, str);
 	//2^strlen(str) ~= 1 << strlen(str)
 	for (int i = 1; i < (1 << strlen(str)); i++)
 	{
@@ -20,4 +43,5 @@ void combinations(char *str)
 		}
 		printf("\n");
 	}
+	free(str);
 }
